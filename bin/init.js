8ec5075c423bbcbf88acb80e7cdf8a7fca51ccf8
mohamednajiub <m.emad550@gmail.com>
2020-01-31 20:46:52 +0200

    fix initialization command

--- bin/init.js
+++ bin/init.js
@@ -1,6 +1,7 @@
+#! /usr/bin/env node
 const argv = require('yargs').argv;
 
 let project_name = argv.project_name;
 let theme_name = argv.theme_name;
 
-export {project_name, theme_name};
+module.exports = {project_name, theme_name}
