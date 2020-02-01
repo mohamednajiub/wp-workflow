846606eea1d198bc078b5d61606b00493742400d
mohamednajiub <m.emad550@gmail.com>
2020-02-01 08:26:18 +0200

    add .vscode settings

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,9 @@
 import {
-    src,
-    dest,
-    watch,
-    series,
-    parallel
+	src,
+	dest,
+	watch,
+	series,
+	parallel
 } from 'gulp';
 
 /********** global tools **********/
@@ -20,7 +20,10 @@ import sourcemaps from 'gulp-sourcemaps';
 // rename used to change name of files
 import rename from "gulp-rename";
 
-import {theme_name, project_name} from './bin/init';
+import {
+	theme_name,
+	project_name
+} from './bin/init';
 /********** style tools **********/
 // gulp sass used to convert sass to css
 import sass from 'gulp-sass';
@@ -45,13 +48,8 @@ const production = yargs.argv.prod;
 
 // create server
 let server = browserSync.create(),
-    // reload browsers
-    reload = server.reload();
-
-// source map initializing
-let sourcemaps_init = sourcemaps.init({
-    loadMaps: true
-});
+	// reload browsers
+	reload = server.reload();
 
 /********** Theme initialization **********/
 // let project_name = 'Xact';
@@ -63,46 +61,46 @@ let root = `../${theme_name}`;
 let php_files = `${root}/**/*.php`;
 
 let styles_src = `${root}/src/sass`,
-    style_files = `${styles_src}/**/*.scss`,
-    css_dest = `${root}/css/`;
+	style_files = `${styles_src}/**/*.scss`,
+	css_dest = `${root}/css/`;
 
 let js_src = `${root}/src/scripts`,
-    js_files = `${js_src}/**/*.js`,
-    js_dest = `${root}/js`;
+	js_files = `${js_src}/**/*.js`,
+	js_dest = `${root}/js`;
 
 let images_src = `${root}/src/images`,
-    image_files = `${images_src}/**/*.{jpg,jpeg,png,svg,gif}`,
-    images_dest = `${root}/images`;
+	image_files = `${images_src}/**/*.{jpg,jpeg,png,svg,gif}`,
+	images_dest = `${root}/images`;
 
 /********** styles function **********/
 export const styles = () => {
-    return src(`${styles_src}/main.scss`, {
-            allowEmpty: true
-        })
-        .pipe(gulpif(!production, sourcemaps.init({
-            loadMaps: true
-        })))
-        .pipe(sass({
-            outputStyle: 'expanded',
-            errLogToConsole: true
-        }).on('error', sass.logError))
-        .pipe(autoPrefixer({
-            grid: true,
-            browsers: [
-                'last 5 chrome version',
-                'last 5 firefox version',
-                'last 5 safari version',
-                'last 5 ie version'
-            ]
-        }))
-        .pipe(gulpif(!production, sourcemaps.write(`/`)))
-        .pipe(dest(css_dest))
-        .pipe(gulpif(production, cleanCss({
-            compatibility: 'ie8'
-        })))
-        .pipe(gulpif(production, rename('main.min.css')))
-        .pipe(gulpif(production, dest(css_dest)))
-        .pipe(server.stream());
+	return src(`${styles_src}/main.scss`, {
+			allowEmpty: true
+		})
+		.pipe(gulpif(!production, sourcemaps.init({
+			loadMaps: true
+		})))
+		.pipe(sass({
+			outputStyle: 'expanded',
+			errLogToConsole: true
+		}).on('error', sass.logError))
+		.pipe(autoPrefixer({
+			grid: true,
+			browsers: [
+				'last 5 chrome version',
+				'last 5 firefox version',
+				'last 5 safari version',
+				'last 5 ie version'
+			]
+		}))
+		.pipe(gulpif(!production, sourcemaps.write(`/`)))
+		.pipe(dest(css_dest))
+		.pipe(gulpif(production, cleanCss({
+			compatibility: 'ie8'
+		})))
+		.pipe(gulpif(production, rename('main.min.css')))
+		.pipe(gulpif(production, dest(css_dest)))
+		.pipe(server.stream());
 }
 
 // export const copy_styles = () => {
@@ -114,79 +112,79 @@ export const styles = () => {
 
 /********** scripts function **********/
 export const scripts = () => {
-    return src(`${js_src}/main.js`)
-        .pipe(webpack({
-            module: {
-                rules: [{
-                    test: /\.js$/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: []
-                        }
-                    }
-                }]
-            },
-            mode: production ? 'production' : 'development',
-            devtool: !production ? 'source-map' : false,
-            output: {
-                filename: production ? 'main.bundle.min.js' : 'main.bundle.js'
-            },
-        }))
-        .pipe(gulpif(production, stripDebug()))
-        .pipe(dest(js_dest));
+	return src(`${js_src}/main.js`)
+		.pipe(webpack({
+			module: {
+				rules: [{
+					test: /\.js$/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: []
+						}
+					}
+				}]
+			},
+			mode: production ? 'production' : 'development',
+			devtool: !production ? 'source-map' : false,
+			output: {
+				filename: production ? 'main.bundle.min.js' : 'main.bundle.js'
+			},
+		}))
+		.pipe(gulpif(production, stripDebug()))
+		.pipe(dest(js_dest));
 }
 
 /********** images function **********/
 export const images = () => {
-    return src(`${images_src}/**/*`)
-        .pipe(changed(images_dest))
-        .pipe(imagemin([
-            imagemin.gifsicle({
-                interlaced: true
-            }),
-            imagemin.mozjpeg({
-                quality: 75,
-                progressive: true
-            }),
-            imagemin.optipng({
-                optimizationLevel: 5
-            }),
-            imagemin.svgo({
-                plugins: [{
-                        removeViewBox: true
-                    },
-                    {
-                        cleanupIDs: false
-                    }
-                ]
-            })
-        ]))
-        .pipe(dest(images_dest))
+	return src(`${images_src}/**/*`)
+		.pipe(changed(images_dest))
+		.pipe(imagemin([
+			imagemin.gifsicle({
+				interlaced: true
+			}),
+			imagemin.mozjpeg({
+				quality: 75,
+				progressive: true
+			}),
+			imagemin.optipng({
+				optimizationLevel: 5
+			}),
+			imagemin.svgo({
+				plugins: [{
+						removeViewBox: true
+					},
+					{
+						cleanupIDs: false
+					}
+				]
+			})
+		]))
+		.pipe(dest(images_dest))
 }
 
 /********** browser sync function **********/
 export const serve = (done) => {
-    server.init({
-        proxy: `http://localhost/${project_name}`,
-        snippetOptions: {
-            ignorePaths: ["wp-admin/**"]
-        },
-    });
-    done();
+	server.init({
+		proxy: `http://localhost/${project_name}`,
+		snippetOptions: {
+			ignorePaths: ["wp-admin/**"]
+		},
+	});
+	done();
 }
 
 export const reload_fun = (done) => {
-    reload;
-    done();
+	reload;
+	done();
 };
 
 /********** watch changes function **********/
 export const watchForChanges = () => {
-    watch(style_files, styles);
-    watch(js_files, series(scripts, reload_fun));
-    watch(image_files, series(images, reload_fun));
-    watch(php_files, reload_fun);
+	watch(style_files, styles);
+	watch(js_files, series(scripts, reload_fun));
+	watch(image_files, series(images, reload_fun));
+	watch(php_files, reload_fun);
 }
 
 export const dev = series(parallel(styles, images, scripts), serve, watchForChanges);
