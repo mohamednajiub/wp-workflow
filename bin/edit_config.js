85c9cbcf6f05cc4b3b4f7014053a9373711b713b
Mohamed Najiub <mohamednajiub@gmail.com>
2020-05-09 05:54:49 +0200

    fix: changing data error

--- bin/edit_config.js
+++ bin/edit_config.js
@@ -8,19 +8,16 @@ const finalization_project = require('./finalize');
 module.exports = () => {
 	prompt(project_prompt)
 		.then(({
-			project_name,
+			project_path,
 			theme_name,
 			server_port
 		}) => {
 			const edit_config = require('./utils');
 			edit_config({
-				project_name,
+				project_path,
 				theme_name,
 				server_port
 			});
-			console.log(`Project Name: ${project_name}`);
-			console.log(`Theme Name: ${theme_name}`);
-			console.log(`Project Port: ${server_port}`);
 			finalization_project()
 		})
 };
