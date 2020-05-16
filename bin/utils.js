85c9ce5845258633d0c861b2432907f83f3b1010
Mohamed Najiub <mohamednajiub@gmail.com>
2020-05-16 18:02:20 +0200

    add creating theme folders function and edit edit config function

--- bin/utils.js
+++ bin/utils.js
@@ -1,14 +1,42 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = (project_data) => {
+const edit_config = (project_data) => {
 	const config_path = path.resolve(process.cwd(), './config.json');
 	const config = require(config_path);
+
 	config.project_path = project_data.project_path;
-	config.theme_name = project_data.theme_name;
+	config.theme_name = path.basename(path.resolve(__dirname));
 	config.server_port = parseInt(project_data.server_port);
 
 	fs.writeFile(config_path, JSON.stringify(config, null, 2), function writeJSON(err) {
 		if (err) return console.log(err);
 	});
+}
+
+const creating_theme_folders = (theme_name) => {
+	fs.mkdirSync(theme_name);
+	let dirs = [
+		`${theme_name}/sass`,
+		`${theme_name}/sass/pages`,
+		`${theme_name}/sass/components`,
+		`${theme_name}/sass/animations`,
+		`${theme_name}/scripts`,
+		`${theme_name}/scripts/pages`,
+		`${theme_name}/scripts/components`,
+		`${theme_name}/images`
+	]
+
+	dirs.forEach((dir) => {
+		fs.mkdirSync(dir, {
+			recursive: true
+		}, (err) => {
+			throw (err)
+		})
+	})
+}
+
+module.exports = {
+	creating_theme_folders,
+	edit_config
 }
