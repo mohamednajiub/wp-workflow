bbdd0d902d0324739352511f3234bbf9ffd1e370
Mohamed Najiub <mohamednajiub@gmail.com>
2020-06-08 02:24:00 +0200

    add babel configuration and fix eslint error in init.js

--- bin/init.js
+++ bin/init.js
@@ -1,11 +1,11 @@
 #! /usr/bin/env node
 
-const fs = require('fs')
-const ora = require('ora')
-const execa = require('execa')
-const process = require('process')
-const chalk = require('chalk')
-const https = require('https')
+const fs = require('fs');
+const ora = require('ora');
+const execa = require('execa');
+const process = require('process');
+const chalk = require('chalk');
+const https = require('https');
 
 module.exports = (theme_name) => {
 	// Files to download
@@ -51,32 +51,32 @@ module.exports = (theme_name) => {
 			file_path:
 				'https://raw.githubusercontent.com/mohamednajiub/wp-workflow/master/config.json',
 		},
-	]
+	];
 
-	process.chdir(`${theme_name}`)
-	const spinner = ora(chalk.blue('2- Loading Package Files\n'))
-	spinner.start()
+	process.chdir(`${theme_name}`);
+	const spinner = ora(chalk.blue('2- Loading Package Files\n'));
+	spinner.start();
 
 	Promise.all(
 		package_files.map((file) => {
-			let created_file = fs.createWriteStream(file.file_name)
+			let created_file = fs.createWriteStream(file.file_name);
 			https.get(file.file_path, function (response) {
-				response.pipe(created_file)
-			})
+				response.pipe(created_file);
+			});
 		})
 	)
 		.then(async () => {
-			spinner.succeed(chalk.green('2- Package files downloaded\n'))
-			spinner.start(chalk.blue('3- installing npm packages\n'))
-			await execa('npm', ['install'])
-			spinner.succeed(chalk.green('3- Installing packages finished successfully\n'))
-			console.log(chalk.blue('4- Set project data\n'))
-			const { edit_config } = require('./questions_handler')
-			edit_config()
+			spinner.succeed(chalk.green('2- Package files downloaded\n'));
+			spinner.start(chalk.blue('3- installing npm packages\n'));
+			await execa('npm', ['install']);
+			spinner.succeed(chalk.green('3- Installing packages finished successfully\n'));
+			console.log(chalk.blue('4- Set project data\n'));
+			const { edit_config } = require('./questions_handler');
+			edit_config();
 		})
 		.catch((error) => {
-			console.log(chalk.white.bgRed(error))
-			console.log('please try again later')
-			execa().cancel()
-		})
-}
+			console.log(chalk.white.bgRed(error));
+			console.log('please try again later');
+			execa().cancel();
+		});
+};
